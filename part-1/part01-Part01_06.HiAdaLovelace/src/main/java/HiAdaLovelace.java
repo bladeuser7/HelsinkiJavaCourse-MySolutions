30f5edeb02e0199d890782430e9695910dcc9ef8
Ace <kalaitzisemmanouil@gmail.com>
2023-08-31 15:21:43 +0200

    part1.06

---
+++ part-1/part01-Part01_06.HiAdaLovelace/src/main/java/HiAdaLovelace.java
@@ -0,0 +1,13 @@
+
+public class HiAdaLovelace {
+
+    public static void main(String[] args) {
+        String name = "Ada Lovelace";
+        String start = "Hi ";
+        String end = "!";
+        
+        System.out.println(start + name + end);
+
+
+    }
+}
