5eb5e29d6ace0526b5d6efb8d6d329ed77ba2891
Ace <kalaitzisemmanouil@gmail.com>
2023-10-31 22:41:21 +0100

    part2.24

---
+++ part-1/part02-Part02_24.FromParameterToOne/src/main/java/FromParameterToOne.java
@@ -0,0 +1,19 @@
+
+
+public class FromParameterToOne {
+
+    public static void main(String[] args) {
+
+
+    }
+    
+    public static void printFromNumberToOne(int number) {
+        int value = 0;
+        while(number > value) {
+            System.out.println(number);
+            number--;
+            
+        }
+    }
+
+}
