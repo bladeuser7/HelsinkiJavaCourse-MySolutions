da7e2389e4ed4e26c968dc48e96196d1613c1955
Ace <kalaitzisemmanouil@gmail.com>
2023-12-07 11:02:37 +0100

    part3.17

---
+++ part-1/part03-Part03_17.RemoveLast/src/main/java/RemoveLast.java
@@ -0,0 +1,32 @@
+
+import java.util.ArrayList;
+
+public class RemoveLast {
+
+    public static void main(String[] args) {
+        // Try your method in here
+        ArrayList<String> strings = new ArrayList<>();
+
+        strings.add("First");
+        strings.add("Second");
+        strings.add("Third");
+        strings.add("Thirdas");
+
+        System.out.println(strings);
+
+        removeLast(strings);
+        removeLast(strings);
+
+        System.out.println(strings);
+    }
+
+    public static void removeLast(ArrayList<String> strings) {
+        int index = (strings.size() - 1);
+        if (strings.size() == 0) {
+            return;
+
+        }
+        strings.remove(index);
+
+    }
+}
