6a5cec83e99420e0522662b73ae9ef7edbaedd33
Ace <kalaitzisemmanouil@gmail.com>
2024-02-13 09:19:07 +0100

    part4.25

---
+++ part-1/part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
@@ -0,0 +1,23 @@
+
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class PrintingASpecifiedFile {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Which file should have its contents printed?");
+        String name = scanner.nextLine();
+        try (Scanner scanner2 = new Scanner(Paths.get(name))) {
+            while (scanner2.hasNextLine()) {
+                String row = scanner2.nextLine();
+                System.out.println(row);
+            }
+        }catch (Exception e) {
+    System.out.println("Error: " + e.getMessage());
+}
+
+    }
+    
+}
