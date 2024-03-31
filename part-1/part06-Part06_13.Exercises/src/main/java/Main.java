48eb4b7789b7bebe36fdc6f2ff5ae5a5a73d4bea
Ace <kalaitzisemmanouil@gmail.com>
2024-03-31 13:27:28 +0200

    part6.13

---
+++ part-1/part06-Part06_13.Exercises/src/main/java/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ace
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Try out your class here
+        
+    }
+
+}
---
+++ part-1/part06-Part06_13.Exercises/src/main/java/MainProgram.java
@@ -0,0 +1,12 @@
+
+public class MainProgram {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int partsCompleted() {
+        return 2;
+        }
+
+}
