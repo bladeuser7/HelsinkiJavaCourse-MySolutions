6bd680f177561e0dbd87f2df02cac54b8ac611d0
Ace <kalaitzisemmanouil@gmail.com>
2024-06-12 11:58:42 +0200

    part11.3

---
+++ part-2/part11-Part11_03.ShowAndTicket/src/main/java/Ticket.java
@@ -0,0 +1,26 @@
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Ace
+ */
+public class Ticket {
+
+    private int seat;
+    private int code;
+    private Show show;
+
+//    public ArrayList<Show> getShow() {
+//        return this.show;
+//    }
+//
+//    public void addShow(Show show) {
+//        this.show.add(show);
+//    }
+}
