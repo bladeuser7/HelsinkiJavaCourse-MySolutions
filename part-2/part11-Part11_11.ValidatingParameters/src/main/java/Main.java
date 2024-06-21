fbf757a8b18375d9a3c0a7b94a5db987f1e1bf7f
Ace <kalaitzisemmanouil@gmail.com>
2024-06-21 12:28:41 +0200

    part11.11

---
+++ part-2/part11-Part11_11.ValidatingParameters/src/main/java/Main.java
@@ -0,0 +1,21 @@
+
+import validating.Person;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ace
+ */
+public class Main {
+    public static void main(String[] args){
+        
+        String nik = "asdaa";
+        Person mike = new Person(nik, 1);
+    }
+    
+}
