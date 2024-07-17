50e544e9bb5abd27e1867ac7d0e4542b0301e159
Ace <kalaitzisemmanouil@gmail.com>
2024-07-17 15:06:28 +0200

    part13.1

---
+++ part-2/part13-Part13_01.MyFirstApplication/src/main/java/myFirstApplication/MyFirstApplication.java
@@ -0,0 +1,20 @@
+package myFirstApplication;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MyFirstApplication extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("My first application");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        launch(MyFirstApplication.class);
+
+    }
+
+}
